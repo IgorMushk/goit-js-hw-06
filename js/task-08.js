3a7cf9935cdbe80ad0f57f7634c92c066d967da7
IgorMushk <igor.mushketik@gmail.com>
2023-06-29 21:49:04 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,51 @@
+/*
 
+Задание 8
+
+Напиши скрипт управления формой логина.
+
+<form class="login-form">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button type="submit">Login</button>
+</form>
+
+1. Обработка отправки формы form.login-form должна быть по событию submit.
+2. При отправке формы страница не должна перезагружаться.
+3. Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
+ что все поля должны быть заполнены.
+4. Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект,
+ где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к
+ элементам формы используй свойство elements.
+5. Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+*/
+
+const formLogin = document.querySelector(".login-form");
+formLogin.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  const valuesOut = {
+    email: "",
+    password: "",
+  };
+
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be filled!");
+    return;
+  }
+
+  valuesOut.email = email.value;
+  valuesOut.password = password.value;
+
+  console.log(valuesOut);
+  event.currentTarget.reset();
+}
